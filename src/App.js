711c309fb5df476f16fe328ecac860ad35b9b543
SakibSaizeed <sakibsaizeed@gmail.com>
2022-08-25 20:47:05 +0600

    Sign-up-form-design with react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { getAuth } from "firebase/auth";
+import app from "./firebase.init";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+const auth = getAuth(app);
 function App() {
+  const handleEmail = (e) => {
+    console.log(e.target.value);
+  };
+  const handlePassword = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    console.log("Submit done");
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="Registration-form w-50 mx-auto mt-5">
+        <h1 className="text-info">Sign Up</h1>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onBlur={handleEmail}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onBlur={handlePassword}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIvGiCsaMN6WVPOXvK4UeWXW9aCCTOL4Q",
+  authDomain: "email-pass-login-c4c1d.firebaseapp.com",
+  projectId: "email-pass-login-c4c1d",
+  storageBucket: "email-pass-login-c4c1d.appspot.com",
+  messagingSenderId: "655668505990",
+  appId: "1:655668505990:web:e16b15e03a6dea32ba5007",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
